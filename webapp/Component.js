eb45192eff4d34e270c6853ab237d295b83aa870
Vinod Patil <vinod.pats@gmail.com>
2018-06-11 18:55:31 +0000

    commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("com.patslistreport1.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("com.patslistreport1.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
